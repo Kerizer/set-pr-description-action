ae6b2b23bcd08b21ab41a16986f16aecab21079c
Pavlo Pakholka <pavlo.pakholka@bolt.eu>
2019-11-01 15:33:38 +0200

    Fetch all commits

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import * as exec from '@actions/exec';
-import fetch from 'node-fetch';
-
-const gitExec = (...gitOptionArray:string[]) => exec.exec('git', [...gitOptionArray]);
+import { gitExec, githubApiHandlerCreator } from './functions';
 
 async function run() {
   try {
+    if (!github.context.payload.pull_request) {
+      throw {
+        message: "This action can only be executed from PR"
+      }
+    }
     const githubApiToken:string = core.getInput('github-token');
     const githubUserName:string = core.getInput('github-user-name');
     const githubUserEmail:string = core.getInput('github-user-email');
 
-    console.log(github.context.payload, { depth: null });
+    // console.log();github.context.payload.
+    const githubApi = githubApiHandlerCreator(github.context.payload, githubApiToken);
+    const prNumber = github.context.payload.pull_request.number;
 
-    if (githubApiToken !== null) {
-      // pullRequestApiUrl = pullRequestApiUrl.replace("api.github.com", `${githubApiToken}@api.github.com`);
-    }
+    const githubCommits = githubApi({resource: `pulls/${prNumber}/commits`});
+
+    console.log('githubCommits: ', JSON.stringify(githubCommits));
 
     if (githubUserName) {
       await gitExec('config', '--global', 'user.name', `"${githubUserName}"`);
