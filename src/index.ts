e8708603e512b03eba1adecc7dc700b23665b54b
Pavlo Pakholka <pavlo.pakholka@bolt.eu>
2019-11-01 13:49:24 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as exec from '@actions/exec';
+import fetch from 'node-fetch';
+
+const gitExec = (...gitOptionArray:string[]) => exec.exec('git', [...gitOptionArray]);
+
+async function run() {
+  try {
+    const githubApiToken:string = core.getInput('github-token');
+    const githubUserName:string = core.getInput('github-user-name');
+    const githubUserEmail:string = core.getInput('github-user-email');
+
+    console.log(github.context.payload);
+
+    if (githubApiToken !== null) {
+      // pullRequestApiUrl = pullRequestApiUrl.replace("api.github.com", `${githubApiToken}@api.github.com`);
+    }
+
+    if (githubUserName) {
+      await gitExec('config', '--global', 'user.name', `"${githubUserName}"`);
+    }
+
+    if (githubUserEmail) {
+      await gitExec('config', '--global', 'user.email', `"${githubUserEmail}"`);
+    }
+
+
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
